602c0728a830da60f4666821ec2a7c806326fd79
Davidhoechtl <davidhoechtl.job@gmail.com>
2025-07-17 15:46:55 +0200

    Refactored the Summarization class and added offloading to the gpu

--- Backend/TextSummaryConverter.py
+++ Backend/TextSummaryConverter.py
@@ -1,74 +1,8 @@
 import time
+import torch
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from transformers import PegasusForConditionalGeneration, PegasusTokenizer
-
-def text_to_tokens(text, tokenizer, max_length):
-    return tokenizer(text, return_tensors="pt", max_length = max_length, truncation=False)["input_ids"]
-
-def chunk_input(text, max_length, tokenizer, stride = 0, minimal_chunk_size = 100):
-    tokens = text_to_tokens(text, tokenizer, max_length)[0]
-    print("Chunking text (tokens: ", len(tokens))
-    chunks = []
-    for i in range(0, len(tokens), max_length - stride):
-        if i + max_length <= len(tokens):
-            chunk_tokens = tokens[i:i+max_length] #substring from pos i to i + max_length
-        else:
-            chunk_tokens = tokens[i:]
-            if len(chunk_tokens) < minimal_chunk_size:
-                break # iscard chunk because it is too small
-
-        chunks.append(chunk_tokens.unsqueeze(0))  # <-- Add batch dimension here
-    return chunks
-
-def text_to_summary(input_ids, tokenizer, model, model_input_limit, summary_max_tokens, summary_min_tokens):
-    # T5 models are trained on a specific prefix for summarization tasks.
-    #preprocessed_text = "summarize: " + text
-
-    # outputs a tensor of shape [batch_size, sequence_length] -> integers that represent the summary
-    summary_ids = model.generate(
-        input_ids,
-        min_length=summary_min_tokens,  # minimum tokens for the summary
-        max_length=summary_max_tokens,  # maximum tokens for the summary
-        num_beams=4,  # higher more comp-cost, better quality
-        no_repeat_ngram_size=3,  # repeating words are forbidden
-        early_stopping=True
-    )
-
-    # Convert integers back to words, use the first
-    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-    return summary
-
-def summarize_text(text, model_name, summary_max_tokens = 150, summary_min_tokens = 100):
-    model_limit = get_input_token_limit(model_name)
-    input_length = len(text.split(' '))
-    print(f"Words of the Text: {input_length}\n")
-
-    tokenizer, model = get_tokenizer_and_model(model_name)
-    start_time = time.time()
-
-    # chunk input and create summaries for each chunk
-    chunks = chunk_input(text, model_limit, tokenizer, stride=100)
-    summaries = []
-    for chunk in chunks:
-        max_len_of_summary = int(model_limit / 3)
-        chunk_summary = text_to_summary(chunk, tokenizer, model, model_limit, max_len_of_summary, 60)
-        summaries.append(chunk_summary)
-        print(chunk_summary+"\n")
-
-    final = summaries[0]
-    if len(summaries) > 1:
-        joined = " ".join(summaries)
-        tokens = text_to_tokens(joined, tokenizer, model_limit)
-        if len(tokens[0]) > model_limit:
-            final = summarize_text(joined, model_name, summary_max_tokens, summary_min_tokens) # still too long -> recursive call
-        else:
-            final = text_to_summary(tokens, tokenizer, model, model_limit, summary_max_tokens, summary_min_tokens) # final summarization
-
-    end_time = time.time()
-    inference_time = end_time - start_time
-    print(f"Inference time: {inference_time:.4f} seconds")
-    return final
 
 def get_input_token_limit(model_name):
     match model_name:
@@ -101,3 +35,80 @@ def get_tokenizer_and_model(model_name):
             peg_tokenizer = PegasusTokenizer.from_pretrained(model_name)
             peg_model = PegasusForConditionalGeneration.from_pretrained(model_name)
             return peg_tokenizer, peg_model
+
+class Summarizer:
+    def __init__(self, model_name):
+        self.model_name = model_name
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        tokenizer, model = get_tokenizer_and_model(model_name)
+        self.tokenizer = tokenizer
+        self.model = model.to(self.device)
+
+
+    def summarize_text(self, text, summary_max_tokens = 150, summary_min_tokens = 100):
+        model_limit = get_input_token_limit(self.model_name)
+        input_length = len(text.split(' '))
+        print(f"Words of the Text: {input_length}\n")
+
+        start_time = time.time()
+
+        # chunk input and create summaries for each chunk
+        chunks = self.chunk_input(text, model_limit, stride=100)
+        summaries = []
+        for chunk in chunks:
+            max_len_of_summary = int(model_limit / 3)
+            chunk_summary = self.text_to_summary(chunk, model_limit, max_len_of_summary, 60)
+            summaries.append(chunk_summary)
+            print(chunk_summary+"\n")
+
+        final = summaries[0]
+        if len(summaries) > 1:
+            joined = " ".join(summaries)
+            tokens = self.text_to_tokens(joined, model_limit)
+            if len(tokens[0]) > model_limit:
+                final = self.summarize_text(joined, summary_max_tokens, summary_min_tokens) # still too long -> recursive call
+            else:
+                final = self.text_to_summary(tokens, model_limit, summary_max_tokens, summary_min_tokens) # final summarization
+
+        end_time = time.time()
+        inference_time = end_time - start_time
+        print(f"Inference time: {inference_time:.4f} seconds")
+        return final
+
+    def text_to_summary(self, input_ids, model_input_limit, summary_max_tokens, summary_min_tokens):
+        # T5 models are trained on a specific prefix for summarization tasks.
+        #preprocessed_text = "summarize: " + text
+
+        # outputs a tensor of shape [batch_size, sequence_length] -> integers that represent the summary
+        summary_ids = self.model.generate(
+            input_ids,
+            min_length=summary_min_tokens,  # minimum tokens for the summary
+            max_length=summary_max_tokens,  # maximum tokens for the summary
+            num_beams=4,  # higher more comp-cost, better quality
+            no_repeat_ngram_size=3,  # repeating words are forbidden
+            early_stopping=True
+        )
+
+        # Convert integers back to words, use the first
+        summary = self.tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+        return summary
+
+    def chunk_input(self, text, max_length, stride=0, minimal_chunk_size=100):
+        tokens = self.text_to_tokens(text, max_length)[0]
+        print("Chunking text (tokens: ", len(tokens))
+        chunks = []
+        for i in range(0, len(tokens), max_length - stride):
+            if i + max_length <= len(tokens):
+                chunk_tokens = tokens[i:i + max_length]  # substring from pos i to i + max_length
+            else:
+                chunk_tokens = tokens[i:]
+                if len(chunk_tokens) < minimal_chunk_size:
+                    break  # iscard chunk because it is too small
+
+            chunks.append(chunk_tokens.unsqueeze(0))  # <-- Add batch dimension here
+        return chunks
+
+    def text_to_tokens(self, text, max_length):
+        return self.tokenizer(text, return_tensors="pt", max_length=max_length, truncation=False)["input_ids"].to(self.device)
+
+
--- Backend/main.py
+++ Backend/main.py
@@ -1,12 +1,13 @@
 import UrlTextConverter
 import TextSummaryConverter
+from TextSummaryConverter import Summarizer
 
 URL = "https://www.handelsblatt.com/finanzen/maerkte/marktberichte/dax-aktuell-dax-legt-auf-ueber-24500-punkte-zu-und-baut-rekord-aus/100140427.html"
 #URL = "https://en.wikipedia.org/wiki/Angela_Merkel"
 #URL = "https://docs.godotengine.org/en/stable/about/introduction.html"
 #URL = "https://www.michaellaitman.com/science/why-are-we-here-what-is-the-meaning-of-life-why-do-we-exist-why-am-i-asking-these-questions/"
 #URL = "https://club.autodoc.co.uk/magazin/how-does-clutch-work"
-#URL = "https://theengineeringblog.com/cylinders-basics-of-desing-function-and-construction/"
+URL = "https://theengineeringblog.com/cylinders-basics-of-desing-function-and-construction/"
 
 text = UrlTextConverter.extract_text_from_url(URL)
 print(text)
@@ -21,6 +22,8 @@ print()
 # print("pegasus-xsum: ", summary_pegasus)
 # print()
 
-summary_bart_large_cnn = TextSummaryConverter.summarize_text(text, "facebook/bart-large-cnn")
+summarizer = Summarizer("facebook/bart-large-cnn")
+summary_bart_large_cnn = summarizer.summarize_text(text)
+summary_bart_large_cnn = summarizer.summarize_text(text)
 print("pegasus-xsum: ", summary_bart_large_cnn)
 print()
